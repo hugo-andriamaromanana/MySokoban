2f528826790e63b3a7aa0d2963896b399f55748a
hugo.andriamaromanana@laplateforme.io <hugo.andriamaromanana@laplateforme.io>
2023-05-30 07:03:29 +0200

    [ADD] Blocks, Plates

--- src/generator/map.py
+++ src/generator/map.py
@@ -10,14 +10,30 @@ class Map():
         self.grid = [row[:-1]+[1] for row in self.grid]
         self.grid[0] = [1 for _ in range(size)]
         self.grid[-1] = [1 for _ in range(size)]
-        self.add_obstacles(self.select_level(current_map))
+        self.add_obstacles(self.select_map(current_map))
+        self.add_blocks(self.select_blocks(current_map))
+        self.add_plates(self.select_plates(current_map))
 
-    def select_level(self,current_map: str):
+    def select_map(self,current_map: str):
         return get_json(path.join(path.dirname(__file__), '..', '..', 'src', 'generator', 'base', 'obstacles.json'))[current_map]
+    
+    def select_blocks(self,current_map: str):
+        return get_json(path.join(path.dirname(__file__), '..', '..', 'src', 'generator', 'base', 'blocks.json'))[current_map]
+    
+    def select_plates(self,current_map: str):
+        return get_json(path.join(path.dirname(__file__), '..', '..', 'src', 'generator', 'base', 'plates.json'))[current_map]
     
     def add_obstacles(self,obstacles: list):
         for obstacle in obstacles:
-            self.grid[obstacle[0]][obstacle[1]] = 1
+            self.grid[obstacle[0]][obstacle[-1]] = 1
+    
+    def add_blocks(self,blocks: list):
+        for block in blocks:
+            self.grid[block[0]][block[-1]] = 2
+    
+    def add_plates(self,plates: list):
+        for plate in plates:
+            self.grid[plate[0]][plate[-1]] = 3
 
     def print_map(self):
         for row in self.grid:
